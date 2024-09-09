0f1c4d09db10a8f32893d3a3597fd350b1f9af69
nzzima <nikita-krylov-2012@mail.ru>
2024-09-09 11:17:02 +0300

    Delete comments code

--- VkontakteNetwork/View/AppView.swift
+++ VkontakteNetwork/View/AppView.swift
@@ -11,28 +11,7 @@ import SDWebImageSwiftUI
 
 struct AppView: View {
     
-//    @EnvironmentObject var loginViewModel: LoginViewModel
-//    @ObservedObject var friendsViewModel = FriendsViewModel()
-//    @State var friends = [Friend]()
-    
     var body: some View {
-//        ZStack {
-//            ScrollView(.vertical) {
-//                LazyVStack {
-//                    ForEach(friends, id: \.self) { friend in
-//                        FriendItem(name: friend.firstName, surname: friend.lastName, photo: friend.photo)
-//                    }
-//                }
-//                .padding(10)
-//            }
-//        }
-//        .padding(.top, 50)
-//        .onAppear{
-//            friendsViewModel.getFriends(token: loginViewModel.token) {friends in
-//                self.friends = friends
-//                print(friends)
-//            }
-//        }
         TabView {
             FriendsView()
                 .tag("1")
@@ -52,26 +31,3 @@ struct AppView: View {
         }
     }
 }
-
-//struct FriendItem: View {
-//    var name: String
-//    var surname: String
-//    var photo: String
-//    
-//    var body: some View{
-//        HStack{
-//            WebImage(url: URL(string: photo))
-//                .resizable()
-//                .frame(width: 50, height: 50)
-//                .clipShape(Circle())
-//            VStack(alignment: .leading) {
-//                Text(name)
-//                    .font(.system(size: 18, weight: .black))
-//                    .padding(.bottom, 2)
-//                Text(surname)
-//            }
-//        }
-//        .frame(maxWidth: .infinity, alignment: .leading)
-//        .padding(.bottom, 8)
-//    }
-//}
