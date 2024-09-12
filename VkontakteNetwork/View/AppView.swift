fddde5a3d70a07750eac3cc33ad46298468e6296
nzzima <nikita-krylov-2012@mail.ru>
2024-09-12 11:59:17 +0300

    Integrate animated TabBar with profile icon-button

--- VkontakteNetwork/View/AppView.swift
+++ VkontakteNetwork/View/AppView.swift
@@ -6,28 +6,94 @@
 //
 
 import SwiftUI
-import SDWebImage
-import SDWebImageSwiftUI
 
 struct AppView: View {
+    @State var selectTab = "Friends"
+    let tabs = ["Friends","Groups","Photos","Profile"]
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
     
     var body: some View {
-        TabView {
-            FriendsView()
-                .tag("1")
-                .tabItem {
-                    Text("Friends")
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectTab) {
+                    FriendsView()
+                        .tag("Friends")
+                    GroupsView()
+                        .tag("Groups")
+                    PhotosView()
+                        .tag("Photos")
+                    Text("Profile")
+                        .tag("Profile")
+            }
+            HStack {
+                ForEach(tabs, id: \.self) {tab in
+                    Spacer()
+                    TabBarItem(tab: tab, selected: $selectTab)
+                    Spacer()
                 }
-            GroupsView()
-                .tag("2")
-                .tabItem {
-                    Text("Groups")
-                }
-            PhotosView()
-                .tag("3")
-                .tabItem {
-                    Text("Photos")
-                }
+            }
+            .padding(.top, 15)
+            .padding(.bottom, 15)
+            .frame(maxWidth: .infinity)
+            .background(Color("MainBG"))
         }
+    }
+}
+
+struct TabBarItem: View {
+    @State var tab: String
+    @Binding var selected: String
+    var body: some View {
+        if tab == "Profile" {
+            Button {
+                withAnimation(.spring()) {
+                    selected = tab
+                }
+            } label: {
+                ZStack {
+                    Circle()
+                        .frame(width: 35, height: 35)
+                        .foregroundColor(selected == tab ? Color("SolidBG") : .white)
+                    Image("Profile")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+        } else {
+            ZStack {
+                Button {
+                    withAnimation(.spring()) {
+                        selected = tab
+                    }
+                    
+                } label: {
+                    HStack {
+                        Image(tab)
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        if selected == tab {
+                            Text(tab)
+                                .font(.system(size: 14))
+                                .foregroundStyle(.black)
+                        }
+                    }
+                }
+            }
+            .padding(.vertical, 5)
+            .padding(.horizontal, 17)
+            .opacity(selected == tab ? 1 : 0.7)
+            .background(selected == tab ? .white : Color("MainBG"))
+            .clipShape(Capsule())
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
     }
 }
